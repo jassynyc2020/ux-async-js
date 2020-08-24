f48bdf4c58218c6b6a36df72ae8c31189baf3243
Hubot <nishihaider@yahoo.com>
2020-08-24 13:42:58 -0400

    done

--- code.js
+++ code.js
@@ -4,13 +4,14 @@ window.onload = function () {
     .addEventListener("click", searchGithub);
 
   function searchGithub() {
-    const url = "https://api.github.com/search/users?q=UserText";
+    const url = "https://api.github.com/search/users?q=";
     const searchUserText = document.querySelector("#searchUser").value;
     const ul = document.getElementById("users");
 
-    fetch(url)
+    fetch(url + searchUserText)
       .then((response) => response.json())
-      .then((result) => console.log(result));
+      .then((result) => {renderUserList(result.items)
+    })
     // Obtain a list of users from the Github API that match searchUserText
     //  The final result will contain an array under the key 'items'
     // Pass this array to `renderUserList`
@@ -21,11 +22,10 @@ window.onload = function () {
     for (let i = 0; i < githubUsers.length; i++) {
       let githubUser = githubUsers[i];
       html += "<li>";
-      html += `<strong>${githubUser.login}</strong>`;
-      html += `<a target="_blank" href="${githubUser.html_url}">`;
+      html += `<img src=${githubUser.avatar_url} class="user_avatar">`
+      html += `<a target="_blank" href="${githubUser.html_url}"<strong>${githubUser.login}</strong></a>`;
       html += "</li>";
       
-      append(ul,li);
     }
     html += "</ul>";
 
