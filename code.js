fd98cd9f3f0aa181d0fa004bb5f78cdebbbd3825
Hubot <nishihaider@yahoo.com>
2020-08-23 23:22:41 -0400

    not done

--- code.js
+++ code.js
@@ -1,27 +1,34 @@
-window.onload = function () { // Run this once the page has loaded.
-    // search
-    document.querySelector("#searchButton").addEventListener("click", searchGithub);
-    function searchGithub() {
-        const url = "https://api.github.com/search/users?q="
-        const searchUserText = document.querySelector("#searchUser").value;
+window.onload = function () {
+  document
+    .querySelector("#searchButton")
+    .addEventListener("click", searchGithub);
 
-        // Obtain a list of users from the Github API that match searchUserText
-        //  The final result will contain an array under the key 'items'
-        // Pass this array to `renderUserList`
+  function searchGithub() {
+    const url = "https://api.github.com/search/users?q=UserText";
+    const searchUserText = document.querySelector("#searchUser").value;
+    const ul = document.getElementById("users");
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => console.log(result));
+    // Obtain a list of users from the Github API that match searchUserText
+    //  The final result will contain an array under the key 'items'
+    // Pass this array to `renderUserList`
+
+  function renderUserList(githubUsers) {
+    let html = "";
+    html += "<ul>";
+    for (let i = 0; i < githubUsers.length; i++) {
+      let githubUser = githubUsers[i];
+      html += "<li>";
+      html += `<strong>${githubUser.login}</strong>`;
+      html += `<a target="_blank" href="${githubUser.html_url}">`;
+      html += "</li>";
+      
+      append(ul,li);
     }
+    html += "</ul>";
 
-    function renderUserList(githubUsers) {
-        let html = "";
-        html += "<ul>";
-        for (let i = 0; i < githubUsers.length; i++) {
-            let githubUser = githubUsers[i];
-            html += "<li>";
-            html += `<strong>${githubUser.login}</strong>`;
-            html += `<a target="_blank" href="${githubUser.html_url}">`;
-            html += "</li>";
-        }
-        html += "</ul>";
-
-        document.querySelector("#resultsContainer").innerHTML = html;
-    }
-}
+    document.querySelector("#resultsContainer").innerHTML = html;
+  }
+}};
